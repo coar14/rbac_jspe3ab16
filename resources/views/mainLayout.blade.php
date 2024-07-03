67ba7be3c877fd71e0036ba4a9c41071cc750ce1
coar <m.masong14@gmail.com>
2024-07-03 08:08:03 +0800

    updated layout for homepage, main, logout

--- resources/views/mainLayout.blade.php
+++ resources/views/mainLayout.blade.php
@@ -6,52 +6,89 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMbDRk5b3lvbfZ2VfQv5ZRJ6pSWo1FnkRJQ07N4" crossorigin="anonymous">
     <style>
         * {
             font-family: calibri;
-            font-size: 1.3rem;
+            font-size: 1.2rem;
         }
 
         .auth-labels {
-              display:inline-block;
+              display: inline-block;
               width: 8em;
         }
 
         .auth-textbox {
-            /* display: inline-block; */
             margin-bottom: .5em;
+        }
+
+        .sidebar {
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+            overflow-y: auto;
+        }
+
+        .content {
+            margin-left: 25%;
+            padding: 2rem;
         }
     </style>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col text-end">
-                <div class="fs-6">
-                    @if(Auth::check())
-                       {{ Auth::user()->userInfo->user_firstname.' '.Auth::user()->userInfo->user_lastname }}
-                       <span class="fs-6" style="font-weight: bold;">
-                       @if(Auth::user()->hasRole('admin'))
-                          : Admin User
-                       @else
-                          : User
-                       @endif
-                       </span>
-                       @include('slugs.logout')
+            @if(Auth::check())
+            <div class="col-md-3 bg-light sidebar p-4">
+                <h2 class="mb-4">
+                    {{ Auth::user()->userInfo->user_firstname.' '.Auth::user()->userInfo->user_lastname }}
+                </h2>
+                <span class="fs-6">
+                    @if(Auth::user()->hasRole('admin'))
+                        : Admin User
+                    @else
+                        : User
                     @endif
-                 </div>
-            </div>
-        </div>
+                </span>
+                <ul class="nav flex-column">
+                    @if(Auth::user()->hasRole('admin'))
+                    <li class="nav-item mb-2">
+                        <a href="{{ route('dash') }}" class="nav-link text-dark">Dashboard</a>
+                    </li>
+                    @endif
 
-        <div class="row">
-            <div class="col">
-                @if(!Auth::check())
-                    @yield('auth-content')
-                @else
-                    @yield('page-content')
-                @endif
+                    <li class="nav-item mb-2">
+                        <a href="{{ route('acctg') }}"
+                            @unless(Auth::user()->hasRole('admin') || Auth::user()->hasRole('bookeeper') || Auth::user()->hasRole('auditor') || Auth::user()->hasRole('audasst'))
+                                class="nav-link text-secondary disabled"
+                            @else
+                                class="nav-link text-dark"
+                            @endunless
+                        >Accounting</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="{{ route('prod') }}"
+                            @unless(Auth::user()->hasRole('admin') || Auth::user()->hasRole('assembler'))
+                                class="nav-link text-secondary disabled"
+                            @else
+                                class="nav-link text-dark"
+                            @endunless
+                        >Production</a>
+                    </li>
+
+                    <li class="nav-item mb-2">
+                        @include('slugs.logout')
+                    </li>
+                </ul>
             </div>
+            <div class="col-md-9 content">
+                @yield('page-content')
+            </div>
+            @else
+            <div class="col">
+                @yield('auth-content')
+            </div>
+            @endif
         </div>
     </div>
 </body>
